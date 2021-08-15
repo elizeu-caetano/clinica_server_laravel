2dd9c49b784a77da2991444bc4c2ac4d2cacea2c
elizeu-caetano <elizeucaetano@outlook.com>
2021-08-14 21:00:49 -0400

    Created Model and Moigration for Default Occupation

---
+++ app/Models/JobMed/DefaultOccupation.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models\JobMed;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use OwenIt\Auditing\Contracts\Auditable as AuditableContract;
+use OwenIt\Auditing\Auditable;
+use App\Traits\AuditTrait;
+
+class DefaultOccupation extends Model implements AuditableContract
+{
+    use HasFactory, Auditable, AuditTrait;
+
+    protected $fillable = ['uuid', 'name', 'code', 'active', 'deleted'];
+
+    public function generateTags(): array
+    {
+        return [
+            'Funções Padrão'
+        ];
+    }
+
+    public function audit()
+    {
+        return $this->morphMany(Audit::class, 'auditable');
+    }
+}
---
+++ database/migrations/2021_08_14_205228_create_default_occupations_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDefaultOccupationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('default_occupations', function (Blueprint $table) {
+            $table->id();
+            $table->uuid('uuid');
+            $table->string('name', 100);
+            $table->unsignedBigInteger('code');
+            $table->boolean('active')->default(true);
+            $table->boolean('deleted')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('default_occupations');
+    }
+}
