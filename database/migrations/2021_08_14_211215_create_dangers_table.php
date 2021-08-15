3a468dbb3d84106df45d839d7349c7c6a98e919d
elizeu-caetano <elizeucaetano@outlook.com>
2021-08-14 21:16:08 -0400

    Created Model and Migration for Danger

---
+++ database/migrations/2021_08_14_211215_create_dangers_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDangersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('dangers', function (Blueprint $table) {
+            $table->id();
+            $table->uuid('uuid');
+            $table->string('name', 100);
+            $table->boolean('active')->default(true);
+            $table->boolean('deleted')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('dangers');
+    }
+}
